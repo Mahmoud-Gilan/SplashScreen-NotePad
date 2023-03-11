cfe08e9136a641c396b183f24876e586ffc18673
Mahmoud-Gilan <97368208+Mahmoud-Gilan@users.noreply.github.com>
2023-03-11 12:38:36 +0200

    Update compon.dart

--- lib/share/compon.dart
+++ lib/share/compon.dart
@@ -37,36 +37,6 @@ Widget MyTextFormField({
     ),
   );
 }
-class MyClipper extends CustomClipper <Path> {
-  @override
-  Path getClip(Size size) {
-    final path = Path();
-    path.lineTo(0, size.height / 1.25);
-    path.cubicTo(
-        size.width / 2 * 0.6,
-        size.height * 0.5,
-        size.width * 2 / 2,
-        size.height * 1.2,
-        size.width * 1.2,
-        size.height / 1.5);
-    path.lineTo(size.width, 0);
-    return path;
-  }
 
-  @override
-  bool shouldReclip(covariant CustomClipper<Path> oldClipper) => false;
 
-}
-
-Future MyNavigator ({
-  required BuildContext context ,
-  required Widget widget,
-}){
-  return Navigator.push(
-    context,
-    MaterialPageRoute(builder: (context){
-      return widget ;
-    }),
-  );
-}
 
